e646c37913523ed5a1261e74f558dba583cc91df
Eftron-Rato <efto7986@gmail.com>
2025-08-19 16:50:53 +0200

    * Today is 19.08.25 2

--- python 10 del/lection10.py
+++ python 10 del/lection10.py
@@ -6,6 +6,15 @@ import json
 with open("data.json", 'r') as file :
     data = json.load(file)
     print(data[0] ["name"])
+    data.append({
+        "name" : "Petar Mijatovic",
+        "age" : 25,
+        "height" : 185,
+        "gender" : "male"
 
-
-
+    })
+    print(data)
+with open("data.json", 'w') as file :
+    json.dump(data,
+              file,
+              indent=1)
