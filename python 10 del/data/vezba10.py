53cba173661acdc5c0a929a9a242bf4cda436998
Eftron-Rato <efto7986@gmail.com>
2025-08-19 20:07:48 +0200

    neznam

---
+++ python 10 del/data/vezba10.py
@@ -0,0 +1,7 @@
+# Napravi 5 proizvoda unutar products.json i ucitati ih u vezba.py
+# Ova se knigi za osnovno skolo, nisam imao odea za drugi proizvodi....
+
+import json
+
+with open ("data/products.json", 'r') as file :
+    print(file)
