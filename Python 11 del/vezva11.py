bbc6e169205e820996967744c9d4399ab12c77f5
Eftron-Rato <efto7986@gmail.com>
2025-08-30 16:57:50 +0200

    do 11.10 sum,

--- Python 11 del/vezva11.py
+++ Python 11 del/vezva11.py
@@ -1,1 +1,20 @@
-# Vezba 11 Git hub ke otvoram novo !
+
+import json
+
+
+# Budzet
+# Dodavanje troskova
+# brisanje troskova
+# logovanje troskova
+
+user = None
+maxi_buget = 100000
+with open('data/user.json', 'r') as file :
+    user = json.load(file)
+
+print(user['buget'])
+
+if user['buget'] >= maxi_buget or user['buget'] <= 0 :
+    print('Desila se greshka, vash buget je veci od dozvolenog ili je premali')
+    exit()
+print("vash bodget iznosi { user ['buget'] }")
