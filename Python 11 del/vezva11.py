8305580357aa2703580de4c1b4cf792fb7f51906
Eftron-Rato <efto7986@gmail.com>
2025-08-30 21:56:07 +0200

    do 11.10 sum, del 2.1

--- Python 11 del/vezva11.py
+++ Python 11 del/vezva11.py
@@ -8,13 +8,34 @@ import json
 # logovanje troskova
 
 user = None
-maxi_buget = 100000
+maxi_buget = 1000000
 with open('data/user.json', 'r') as file :
     user = json.load(file)
 
-print(user['buget'])
+user_buget = user["buget"] + user["credit"]
 
-if user['buget'] >= maxi_buget or user['buget'] <= 0 :
+
+print(user_buget) # buget + credit
+
+if user_buget >= maxi_buget or user_buget <= 0 :
     print('Desila se greshka, vash buget je veci od dozvolenog ili je premali')
     exit()
-print("vash bodget iznosi { user ['buget'] }")
+print(f"vash budget iznosi  {user_buget} ")
+
+expense = 0
+
+
+while expense <= 0 or expense > user_buget :
+    expense = int(input("Molim Vnesite iznos troshka :  "))
+
+with open("logs/expense_log.txt", 'a') as file :
+    remaining_budget = user_buget-expense
+    message = f" \n Amaunt : {expense}, User : {user["id"]}, Buged : {user_buget}, Preostali buget {remaining_budget}"
+    file.write(message)
+
+
+
+
+#napravite tex file expens log.txt
+#upisite svaki troshak u sledecem format
+#"Amaunt : Cifra
