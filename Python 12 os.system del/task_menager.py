9fcc7c7e3a8d73eba56cd89c7a31d03af9d51095
Eftron-Rato <efto7986@gmail.com>
2025-09-01 20:42:29 +0200

    lekcija 12,12 del 2

--- Python 12 os.system del/task_menager.py
+++ Python 12 os.system del/task_menager.py
@@ -1,7 +1,14 @@
 import psutil
+import time
 
-processes = psutil.process_iter(['name', 'memory_percent', 'cpu_percent'])
-
+processes = list(psutil.process_iter(['name', 'memory_percent', 'cpu_percent']))
 
 for proc in processes :
-    print(f"name {proc.name()} / Memoriy : {proc.memory_percent():.2f}% / CPU :  {proc.cpu_percent(interval=None)} % ")
+    proc.cpu_percent(interval=None)
+
+time.sleep(1)
+
+print(f"{'Name':<25}{'Memory':<15}{'CPU':<15}")
+
+for proc in processes :
+    print(f"{proc.name() : <10}{proc.memory_percent():<10.2f}{proc.cpu_percent():<10}")
