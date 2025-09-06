9f1bfa422b84e5635aeccf53d344cbca77ebab0a
Eftron-Rato <efto7986@gmail.com>
2025-09-06 11:39:06 +0200

    del 14 RegEx

--- Python 14 del RegEx/vezba14.py
+++ Python 14 del RegEx/vezba14.py
@@ -1,3 +1,13 @@
 
  #Regex koji ce proveravati dali ima samo slova unatar stringa
+import re
 
+
+
+sentence = "I love Python"
+letter_pattern = r"^[a-zA-Z ]+$"
+
+if re.match(letter_pattern, sentence):
+    print("Ima samo bukvi")
+else :
+    print("ima i broevi")
