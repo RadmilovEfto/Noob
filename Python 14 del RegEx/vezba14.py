0a3c2abf69558a165736d826851c17f24f2ba141
Eftron-Rato <efto7986@gmail.com>
2025-09-08 20:23:12 +0200

    Domashna 14

--- Python 14 del RegEx/vezba14.py
+++ Python 14 del RegEx/vezba14.py
@@ -23,3 +23,22 @@ if re.match(capital_pattern,important_sentence) :
     print("String pocinje sa veliko slovo")
 else :
     print("String Pocinje sa malo slovo")
+
+phone_nummber = "06764238244"
+# 0678, 0677, 0660,
+phone_pattern = r"^067(6|7|0)"
+
+phone_match = re.match(phone_pattern, phone_nummber)
+
+phone_map = {
+    "0676" : "Linz",
+    "0677" : "Tirol",
+    "0660" : "Salzburg"
+
+}
+
+
+if phone_match :
+    prefix = "067"+phone_match.group(1)
+
+    print(f"Prefix number is {prefix} and country is {phone_map[prefix]} ")
