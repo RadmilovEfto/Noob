df998873018403ba7f854548e63f94c24ab51d3b
Eftron-Rato <efto7986@gmail.com>
2025-09-13 09:54:28 +0200

    # lekcija 15.

--- Python 15 del RegEx 2/analyzer.py
+++ Python 15 del RegEx 2/analyzer.py
@@ -1,11 +1,20 @@
 import re
 
-with open('errors.log', 'r') as file :
+
+with open('logs/http.log', 'r') as file :
     lines = file.readlines()
 
-pattern = r'Error \d{3}'
+error_pattern = r'Error \d{3}'
+success_pattern =r'Status \d{3}'
 
 for line in lines :
-    match = re.search(pattern, line)
-    if match :
-        print(line)
+    if re.search(error_pattern, line) :
+        with open ("logs/errors.log","a") as errors_file :
+            errors_file.write(line)
+
+    elif re.search(success_pattern, line):
+        with open ("logs/success.log","a") as success_file :
+            success_file.write(line)
+
+
+
