73fa1d30b04669d0ca8f92ef1a39c75cb83864f4
Eftron-Rato <efto7986@gmail.com>
2025-09-13 09:57:36 +0200

    # lekcija 15.vezba1

--- Python 15 del RegEx 2/analyzer.py
+++ Python 15 del RegEx 2/analyzer.py
@@ -6,15 +6,13 @@ with open('logs/http.log', 'r') as file :
 
 error_pattern = r'Error \d{3}'
 success_pattern =r'Status \d{3}'
+with open ("logs/errors.log","a") as errors_file , open ("logs/success.log","a") as success_file :
+    for line in lines :
+        if re.search(error_pattern, line) :
+                errors_file.write(line)
 
-for line in lines :
-    if re.search(error_pattern, line) :
-        with open ("logs/errors.log","a") as errors_file :
-            errors_file.write(line)
-
-    elif re.search(success_pattern, line):
-        with open ("logs/success.log","a") as success_file :
-            success_file.write(line)
+        elif re.search(success_pattern, line):
+                success_file.write(line)
 
 
 
