e9679361b43c0d4232da223f93afe40416b85a0b
Eftron-Rato <efto7986@gmail.com>
2025-10-06 20:58:31 +0200

    mySql upgrade with Homework 06.010.25

--- Python 17 MySQL/main.py
+++ Python 17 MySQL/main.py
@@ -1,2 +1,19 @@
 
 
+import pymysql
+
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="radmil0v",
+    database="python17")
+
+if connection.open :
+    print("Connected")
+else :
+    print("not")
+
+cursor = connection.cursor()
+
+cursor.execute("INSERT INTO users (usarname, password, ages) VALUES ('Toma', 'aaass',35) ")
+connection.commit()
