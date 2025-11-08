1f23f0b7527c8e9c2dea85aed624f82a3d9dd3a5
Eftron-Rato <efto7986@gmail.com>
2025-11-08 12:53:58 +0100

    pocetok

---
+++ OOP 1/main.py
@@ -0,0 +1,23 @@
+
+# OOP
+#Klasa
+# Osoba -> ime , prezime, visina, tezina
+  # Tomislav, Nikolic, 183.5, 100 -> Object
+  # Marko, Markovic, 181.5, 120   -> Objekt
+  # Sara, Petrovvic, 170, 70      -> Object
+# Product -> name, size, prize, amount
+# Kalssa moze imate atribute(variable), metode, def, funkcije
+
+
+class Person :
+    def say_hello(self):
+        print("Hello Wrold")
+
+toma = Person()
+toma.say_hello()
+
+
+#Objekt
+# Product -> name, size, prize, amount
+# class Product -> "hleb" 123, 90, 1
+# class Product -> 'kompir' 12, 123, 1
